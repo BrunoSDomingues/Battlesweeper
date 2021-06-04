68abfdd01add41f6717c7ab5283bac232ad45a43
Bruno Domingues <bruno_signorelli@outlook.com>
2021-06-04 14:00:55 -0300

    Added menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        Debug.Log("loading play...");
+        SceneManager.LoadScene("Game");
+    }
+}
